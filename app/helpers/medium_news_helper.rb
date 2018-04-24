267b599389a804d8aadd1ad79a0abb558a29ffe7
Alberto Aragon <alberto@alturasoluciones.com>
2018-04-24 16:42:29 -0500

    Refactoring Medium News

---
+++ app/helpers/medium_news_helper.rb
@@ -0,0 +1,14 @@
+module MediumNewsHelper
+
+  require 'rss'
+  require 'open-uri'
+
+  def medium_news
+    rss = RSS::Parser.parse(open('https://medium.com/feed/alturasoluciones').read, false).items[0..5]
+    rss.map{|article| {
+        title: article.title, date: article.pubDate, link: article.link, description: article.description, creator: article.dc_creator
+    }}
+  end
+
+end
+
